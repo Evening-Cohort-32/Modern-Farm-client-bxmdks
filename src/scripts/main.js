3fe4835fa5287a65665ce3d56866843a58779519
ChardonneDixon <chardonne.dixon@gmail.com>
2026-03-30 19:01:15 -0500

    staging updates to pull from develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,13 @@
 import { createPlan } from "./plan.js"
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
 import { createAsparagus } from "./seeds/asparagus.js"
 import { createCorn } from "./seeds/corn.js"
-=======
-import {createAsparagus} from "./seeds/asparagus.js"
-import {createCorn} from "./seeds/corn.js"
->>>>>>> develop
 import { createPotato } from "./seeds/potato.js"
 import { createSoybean } from "./seeds/soybean.js"
 import { createSunflower } from "./seeds/sunflower.js"
 import { createWheat } from "./seeds/wheat.js"
-<<<<<<< HEAD
 import { Catalog } from "./catalog.js"
-
->>>>>>> Stashed changes
-=======
 import {addPlant, usePlants} from "./field.js"
 
->>>>>>> develop
 const yearlyPlan = createPlan()
 
 const asparagusSeed = createAsparagus()
@@ -42,8 +30,6 @@ console.log(wheatSeed)
 
 const fullHarvest = document.querySelector(".container")
 console.log("Welcome to the main module")
-<<<<<<< HEAD
-=======
 
 //Create seed objects
 const asparagus = createAsparagus();
@@ -54,4 +40,3 @@ const wheat = createWheat();
 addPlant(asparagus);  // Adds 1 asparagus
 addPlant(corn);       // Adds 2 corn (handled by Array.isArray check)
 addPlant(wheat);      // Adds 1 wheat
->>>>>>> develop
